9dbe495d841f80e1617f627ca35b18fc0940b683
Jado <alijkhalil@gmail.com>
2018-03-09 18:34:51 -0500

    Added custom Swish activation (based on Google research) and also a Squeeze and Exicte network block.

--- layers/general.py
+++ layers/general.py
@@ -7,10 +7,12 @@ import warnings, math
 import tensorflow as tf
 
 import keras.backend as K
-from keras.engine.topology import Layer
 from keras.layers.core import Lambda
-from keras.layers.merge import add, _Merge
-
+from keras.engine.topology import Layer
+from keras.layers import Dense, Reshape
+from keras.layers.pooling import GlobalAveragePooling2D
+from keras.layers.merge import add, multiply, _Merge
+from keras.initializers import Constant
 
 
 DEFAULT_EPSILON=1E-5
@@ -193,3 +195,76 @@ def res_add(drop_dict=None):
     
     # Return correct function for adding layers
     return ret_fn
+
+    
+# Layers to return a squeeze/excitation vector or block output 
+# These are capable of being integrated into any CNN and usually result in an improved model 
+# Based on: https://arxiv.org/pdf/1709.01507.pdf    
+#
+# WARNING: Not a real layer object so it cannot be added directly to an RNN 
+def se_block(reduction_ratio=16, num_final_layers=None, only_excite_vec=False):
+    def func(input):        
+        # Get original number of filters
+        channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+        
+        orig_filters = K.int_shape(input)[channel_axis]
+        if num_final_layers is None:
+            final_filters = orig_filters
+        else:
+            final_filters = num_final_layers
+        
+        # Perform spatial dimension reduction
+        squeeze = GlobalAveragePooling2D()(input)
+        squeeze = Reshape((1, 1, orig_filters))(squeeze)
+        
+        # Pass through Dense, ReLU, Dense, Sigmoid sequence (for attention vector)
+        excite = Dense(int(final_filters // reduction_ratio), activation='relu', 
+                        kernel_initializer='he_normal', use_bias=False)(squeeze)
+        excite = Dense(final_filters, activation='sigmoid', kernel_initializer='he_normal', 
+                        use_bias=False)(excite)
+
+        # Multiply each spatial channel by the attention value
+        if only_excite_vec:
+            return excite
+        else:    
+            return multiply([input, excite])
+    
+    return func
+    
+    
+# Custom swish activation (acting on activations per-channel)
+# Based on following papers: https://arxiv.org/pdf/1801.07145.pdf
+#                            https://arxiv.org/pdf/1710.05941.pdf
+#
+# NOTE: Almost a mix of dumbed=down SE block with the derivative effect of a ReLU
+class custom_swish(Layer):
+    def __init__(self, init_multupler=1.0, **kwargs):
+        self.init_multupler = init_multupler
+
+        super(custom_swish, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    
+        # Create trainable scalar and bias variables for the layer.
+        self.scalar = self.add_weight(name='multipler',
+                                      shape=(input_shape[channel_axis], ),
+                                      initializer=Constant(value=self.init_multupler),
+                                      trainable=True)
+                                      
+        self.bias = self.add_weight(name='bias_term',
+                                      shape=(input_shape[channel_axis], ),
+                                      initializer='zeros',
+                                      trainable=True)
+                                      
+        super(custom_swish, self).build(input_shape)
+
+    def call(self, input):
+        # Pass re-scaled L2 distance through Tanh function
+        ret_val = (input * K.sigmoid((self.scalar * input) + self.bias))
+        
+        # Return final values (always between 0 and 1)
+        return ret_val
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
